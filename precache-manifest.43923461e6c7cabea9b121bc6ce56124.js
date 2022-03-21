eb5fe4dd00673313fc23942605f740134bd3ec43
TuanNguyen0805 <tuanna@comartek.vn>
2022-03-21 17:11:31 +0700

    Updates

---
+++ precache-manifest.43923461e6c7cabea9b121bc6ce56124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e61d8f6e50c577873a3c6a6070cf09c",
+    "url": "/LoginForm/index.html"
+  },
+  {
+    "revision": "476968d4d96d189a997b",
+    "url": "/LoginForm/static/js/2.95904b3b.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/LoginForm/static/js/2.95904b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332ab955d27c2b2ce74b",
+    "url": "/LoginForm/static/js/main.8b1bd57a.chunk.js"
+  },
+  {
+    "revision": "8f34917e1438f01d6c7f",
+    "url": "/LoginForm/static/js/runtime-main.ff35b4a7.js"
+  },
+  {
+    "revision": "5a18a18b196622453094ddb23bf30bbb",
+    "url": "/LoginForm/static/media/item8.5a18a18b.png"
+  }
+]);
